3c6961ec40f1805296488c3b2cd829906de24465
janloongdoo <dujianglong@rongzhitong.com>
2018-04-19 16:30:18 +0800

    zipkin init

--- eureka-client2/src/main/java/com/janloong/eurekaclient2/EurekaClient2Application.java
+++ eureka-client2/src/main/java/com/janloong/eurekaclient2/EurekaClient2Application.java
@@ -5,12 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
-//@EnableHystrix
+@EnableHystrix
 @EnableCircuitBreaker
 public class EurekaClient2Application {
 
