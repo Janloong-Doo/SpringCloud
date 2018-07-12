300c6539553f40b33473f7a2850d29672c750dbd
janloongdoo <dujianglong@rongzhitong.com>
2018-07-12 18:05:30 +0800

    。。。。

---
+++ single-spring-security/src/main/java/com/janloong/singlespringsecurity/SingleSpringSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.janloong.singlespringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SingleSpringSecurityApplication.class, args);
+    }
+}
---
+++ single-spring-security/src/main/java/com/janloong/singlespringsecurity/config/WebSecurityConfig.java
@@ -0,0 +1,50 @@
+package com.janloong.singlespringsecurity.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author <a href ="mailto: janloongdoo@gmail.com">Janloong</a>
+ * @date 2018-07-12 16:04
+ */
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+
+    /**
+     * 配置用户认证方式和密码加密方式
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(null)
+                .passwordEncoder(new PasswordEncoder() {
+                    @Override
+                    public String encode(CharSequence charSequence) {
+                        return null;
+                    }
+
+                    @Override
+                    public boolean matches(CharSequence charSequence, String s) {
+                        return false;
+                    }
+                });
+    }
+
+    
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
